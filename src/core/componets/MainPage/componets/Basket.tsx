e314d92727ae17d771c1a0691a6bae060ca4bcca
Wolahi <tort12345y@gmail.com>
2023-05-03 17:12:36 +0300

    BasketCard

--- src/core/componets/MainPage/componets/Basket.tsx
+++ src/core/componets/MainPage/componets/Basket.tsx
@@ -1,14 +1,34 @@
 import styles from "../MainPage.module.scss";
 import Bike from "../../../../assets/MainPage/icons/delivery.svg";
+import BasketCard from "./BasketCard";
+import { StaticImageData } from "next/image";
+import CheeseBurg from "../../../../assets/MainPage/imgs/CheeseBurg.png";
 
-const Header = (): JSX.Element => {
+export interface BasketCart {
+  icon: StaticImageData;
+  name: string;
+  weight: string;
+  cost: Number;
+  count: Number;
+}
+
+const obj: BasketCart = {
+  icon: CheeseBurg,
+  name: "Супер сырный",
+  weight: "512",
+  cost: 550,
+  count: 1,
+};
+const Basket = (): JSX.Element => {
   return (
     <div className={styles.basket}>
       <div className={styles.basketHeader}>
         <span className={styles.textHeader}>Корзина</span>
         <div className={styles.count}>0</div>
       </div>
-      <div className={styles.order}></div>
+      <div className={styles.order}>
+        <BasketCard card={obj} />
+      </div>
       <div className={styles.buyBlock}>
         <div className={styles.buyHead}>
           <div className={styles.buyText}>Итого</div>
@@ -26,4 +46,4 @@ const Header = (): JSX.Element => {
   );
 };
 
-export default Header;
+export default Basket;
---
+++ src/core/componets/MainPage/componets/BasketCard.tsx
@@ -0,0 +1,25 @@
+import styles from "../MainPage.module.scss";
+import CheeseBurg from "../../../../assets/MainPage/imgs/CheeseBurg.png";
+import Image from "next/image";
+import { BasketCart } from "./Basket";
+
+const BasketCard = (props: any): JSX.Element => {
+  const obj = props.card;
+  return (
+    <div className={styles.basketCard}>
+      <Image src={obj.icon} alt="Not Found" className={styles.burgIcon} />
+      <div className={styles.descriptionCard}>
+        <div className={styles.name}>{obj.name}</div>
+        <div className={styles.weight}>{obj.weight}г</div>
+        <div className={styles.cost}>{obj.cost}₽</div>
+      </div>
+      <div className={styles.buttonCard}>
+        <div className={styles.minus}>-</div>
+        <div className={styles.count}>{obj.count}</div>
+        <div className={styles.plus}>+</div>
+      </div>
+    </div>
+  );
+};
+
+export default BasketCard;
